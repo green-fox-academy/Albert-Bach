fc19e4339276f0de0bd9fcd79ffb8a593207bbcc
Albert Bach <bach_albert@hotmail.hu>
2017-09-11 13:40:55 +0200

    Introduction done

---
+++ week-02/day1/IntroduceYourself/IntroduceYourself/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduceYourself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Albert Bach");
+            Console.WriteLine("Age: 26");
+            Console.WriteLine("Height: 1.74m");
+
+            Console.ReadLine();
+
+        }
+    }
+}
