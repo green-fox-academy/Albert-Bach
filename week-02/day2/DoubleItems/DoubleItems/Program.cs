e1ccb722fa5fe11edd2a8457868cdfa19f1fcb97
Albert Bach <bach_albert@hotmail.hu>
2017-09-12 22:36:40 +0200

    DoubleItems done

---
+++ week-02/day2/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < ag.Length; i++)
+            ag[i] *= 2;
+            
+
+            foreach (int i in ag)
+            
+            Console.WriteLine(i);
+            Console.ReadKey();
+
+        }
+    }
+}
