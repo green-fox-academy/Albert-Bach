011c3db6ce393b37cbba7b8039c943c7c8f05336
Albert Bach <bach_albert@hotmail.hu>
2017-09-12 17:01:52 +0200

    Sum Elements done

---
+++ week-02/day2/Sum Elements/Sum Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = { 54, 23, 66, 12 };
+
+            Console.WriteLine(r[1]+ r[2]);
+        
+        
+            {
+            Console.ReadLine();
+            }    
+
+        }
+    }
+}
