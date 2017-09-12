9f3a4572328a2356a32528371d2f3fd317d1601e
Albert Bach <bach_albert@hotmail.hu>
2017-09-12 20:29:41 +0200

    Summing done

---
+++ week-02/day2/DiagonalMatrix/DiagonalMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 4];
+            for (int i = 0; i < array.Rank; i++)
+
+            {
+                total *= array.GetLength(i);
+            }
+            Console.WriteLine("{0} equals {1}", allLength, total);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
