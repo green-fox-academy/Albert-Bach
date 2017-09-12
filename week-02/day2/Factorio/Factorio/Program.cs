070854fc7c20f40471ac71f409db951bd013a307
Albert Bach <bach_albert@hotmail.hu>
2017-09-12 20:46:52 +0200

    Factorio done

---
+++ week-02/day2/Factorio/Factorio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorio
+{
+    class Program
+    {
+            public static int Factorio(int input)
+            {
+                for (int i = input; i > 1; i--)
+                {
+                    input *= (i - 1);
+                }
+                return input;
+            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorio(5));
+            Console.ReadLine();
+        }
+        
+    }
+}
