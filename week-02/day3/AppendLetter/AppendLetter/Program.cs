1ddcd4f8d199f2b57c7022853e02f1465bc9cbfe
Albert Bach <bach_albert@hotmail.hu>
2017-09-13 13:22:32 +0200

    TakesLonger done

---
+++ week-02/day3/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            List<string> words = new string[] { "Hello", "world" }.ToList();
+            words.Insert(0, "Well");
+            Console.WriteLine(far);
+        }
+    }
+}
