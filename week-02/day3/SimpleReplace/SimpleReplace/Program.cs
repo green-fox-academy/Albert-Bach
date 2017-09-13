f5c1bfdf82a895e8c0c6510f780b5c3a617a2e07
Albert Bach <bach_albert@hotmail.hu>
2017-09-13 11:38:19 +0200

    SimpleReplace done

---
+++ week-02/day3/SimpleReplace/SimpleReplace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+
+           
+            Console.WriteLine(example.Replace("dishwasher", "galaxxy"));
+            Console.ReadLine();
+        }
+    }
+}
