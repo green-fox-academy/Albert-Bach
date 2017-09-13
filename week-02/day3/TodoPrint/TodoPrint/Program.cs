e2080106bfb8c2facca1e06e06d4352873113d0a
Albert Bach <bach_albert@hotmail.hu>
2017-09-13 13:57:46 +0200

    TodoPrint done

---
+++ week-02/day3/TodoPrint/TodoPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  todoText =   "My todo:\n  - Buy milk\n  - Download games\n\t  - Diablo";
+
+            Console.WriteLine(todoText);
+            Console.ReadLine();
+        }
+    }
+}
