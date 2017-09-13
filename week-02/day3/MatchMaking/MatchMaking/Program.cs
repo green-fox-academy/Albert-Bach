c74a74cbc3d1b1671331671a696b8aa5d73b8f89
Albert Bach <bach_albert@hotmail.hu>
2017-09-13 16:28:52 +0200

    MatchMaking done

--- week-02/day3/MatchMaking/MatchMaking/Program.cs
+++ week-02/day3/MatchMaking/MatchMaking/Program.cs
@@ -14,10 +14,15 @@ namespace MatchMaking
             var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
             var order = new List<string>();
 
-            // Join the two lists by matching one girl with one boy in the order list
-            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
-
-            Console.WriteLine(order);
+            for (int i = 0; i < girls.Count; i++)
+            {
+                order.Add(girls.ElementAt(i));
+                order.Add(boys.ElementAt(i));
+            }
+            foreach (string s in order)
+            {
+                Console.WriteLine(s);
+            }
             Console.ReadLine();
         }
     }
