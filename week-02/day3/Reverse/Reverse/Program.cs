e602ad9cb4ef0f8af7ddd1e8532c7bad6d94986d
Albert Bach <bach_albert@hotmail.hu>
2017-09-13 15:47:28 +0200

    Reverse  done

--- week-02/day3/Reverse/Reverse/Program.cs
+++ week-02/day3/Reverse/Reverse/Program.cs
@@ -9,15 +9,25 @@ namespace Reverse
     class Program
     {
         static void Main(string[] args)
-        {
 
-            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-
-            // Create a function that can reverse a String, which is passed as the parameter
-            // Use it on this reversed string to check it!
-
-            Console.WriteLine(reversed);
-            Console.ReadLine();
+            {
+                string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+                reversed= ReverseString(reversed);
+                Console.WriteLine(reversed);
+                Console.ReadLine();
+            }
+            static string ReverseString(string str)
+            {
+                string newstr = "";
+                for (int i = 1; i < str.Length; i++)
+                {
+                    newstr = newstr + str[str.Length - i];
+                }
+                newstr = newstr + str[0];
+                return newstr;
+            }
         }
-    }
+        
 }
+    
+
