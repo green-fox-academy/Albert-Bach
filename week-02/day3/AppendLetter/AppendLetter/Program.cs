bd83e7602fa16def5fb19f5ca368af6197bcd3ab
Albert Bach <bach_albert@hotmail.hu>
2017-09-14 09:19:17 +0200

    AppendLetter

--- week-02/day3/AppendLetter/AppendLetter/Program.cs
+++ week-02/day3/AppendLetter/AppendLetter/Program.cs
@@ -10,12 +10,19 @@ namespace AppendLetter
     {
         static void Main(string[] args)
         {
-           var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
-            // Add "a" to every string in the far list.
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
 
-            List<string> words = new string[] { "Hello", "world" }.ToList();
-            words.Insert(0, "Well");
-            Console.WriteLine(far);
+            for (int i = 0; i < far.Count; i++)
+
+            {
+                far[i] += ("a");
+                Console.WriteLine(far[i]);
+
+            }
+
+            Console.ReadLine();
+
+            
         }
     }
 }
