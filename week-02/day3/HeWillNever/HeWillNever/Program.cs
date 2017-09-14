53c86d523396153eb53bac8d8ca37b9f57fc1749
Albert Bach <bach_albert@hotmail.hu>
2017-09-14 23:14:07 +0200

    Rick more simple

--- week-02/day3/HeWillNever/HeWillNever/Program.cs
+++ week-02/day3/HeWillNever/HeWillNever/Program.cs
@@ -10,42 +10,34 @@ namespace HeWillNever
     {
         static void Main(string[] args)
         {
-            string output = "";
-            int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
-
-
-            var map = new Dictionary<int, string>();
-
-            map.Add(7, "run around and desert you");
-            map.Add(50, "tell a lie and hurt you");
-            map.Add(49, "make you cry,");
-            map.Add(2, "let you down");
-            map.Add(12, "give you up,");
-            map.Add(1, "Never gonna");
-            map.Add(11, "\n");
-            map.Add(3, "say goodbye");
-
-            foreach (int lyrics in notSoCrypticMessage)
             {
+                string output = "";
+                int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
 
-                if (map.ContainsKey(lyrics))
+                var map = new Dictionary<int, string>();
+
+                map.Add(7, "run around and desert you");
+                map.Add(50, "tell a lie and hurt you");
+                map.Add(49, "make you cry,");
+                map.Add(2, "let you down");
+                map.Add(12, "give you up,");
+                map.Add(1, "Never gonna");
+                map.Add(11, "\n");
+                map.Add(3, "say goodbye");
+
+                for (int i = 0; i < notSoCrypticMessage.Length; i++)
                 {
-
-                    if (lyrics != 11)
+                    string value;
+                    if (map.TryGetValue(notSoCrypticMessage[i], out value))
                     {
-                        output += (map[lyrics] + " ");
+                        output += value;
+                        if (notSoCrypticMessage[i] != 11) output += " ";
                     }
-                    else
-                    {
-                        output += map[lyrics];
-                    }
-
                 }
 
+                Console.WriteLine(output);
+                Console.ReadLine();
             }
-            Console.WriteLine(output);
-            Console.ReadKey();
-
         }
     }
 }
