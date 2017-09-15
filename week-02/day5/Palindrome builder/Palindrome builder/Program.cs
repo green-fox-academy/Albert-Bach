81f58262bd791c2263b0e59d718b6c6db3a32a11
Albert Bach <bach_albert@hotmail.hu>
2017-09-15 11:34:30 +0200

    Anagram done

---
+++ week-02/day5/Palindrome builder/Palindrome builder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Create a function named create palindrome following your current language's style guide. 
+           // It should take a string, create a palindrome from it and then return it.
+        }
+    }
+}
