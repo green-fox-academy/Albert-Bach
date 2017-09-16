47ce513ac125f0d1c85a70d538462d86218b968b
Albert Bach <bach_albert@hotmail.hu>
2017-09-16 23:29:40 +0200

    Multiplication_Table done

---
+++ week-02/day1/Multiplication_Table/Multiplication_Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write down a number!");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+
+            {
+                Console.WriteLine(i + " * " + number + " = " + i * number);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
