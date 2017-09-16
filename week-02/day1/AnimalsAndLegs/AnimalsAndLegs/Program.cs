dfcd085b33b34fd6a97392552fdfa7cca5375374
Albert Bach <bach_albert@hotmail.hu>
2017-09-16 20:14:17 +0200

    AnimalsAndLegs done

---
+++ week-02/day1/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers of your chickens:");
+            int chickens = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numbers of your pigs:");
+            int pigs = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("The ammount of legs are:" + " " + ((chickens * 2) + (pigs * 4)));
+            Console.ReadLine();
+        }
+    }
+}
