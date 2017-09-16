ee5dfb1f6153ae14cce0e868e32f40f1522f3875
Albert Bach <bach_albert@hotmail.hu>
2017-09-16 19:13:24 +0200

    SecondsInADay done

---
+++ week-02/day1/SecondsinADay/SecondsinADay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondsinADay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            Console.WriteLine(24*60*60 - currentHours * 60 * 60 - currentMinutes * 60 - currentSeconds +" seconds left from the day");
+            Console.ReadLine();
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+        }
+    }
+}
