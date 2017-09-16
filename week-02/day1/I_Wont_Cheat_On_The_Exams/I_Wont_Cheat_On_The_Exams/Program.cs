e00ec70adbdb231d8ac4429851f258873e33a165
Albert Bach <bach_albert@hotmail.hu>
2017-09-16 23:16:11 +0200

    I_Wont_Cheat_On_The_Exams done

---
+++ week-02/day1/I_Wont_Cheat_On_The_Exams/I_Wont_Cheat_On_The_Exams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Wont_Cheat_On_The_Exams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
