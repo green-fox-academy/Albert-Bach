fd7fd88dde35928a1daa0166b76a893d24e154b4
Albert Bach <bach_albert@hotmail.hu>
2017-09-20 11:51:49 +0200

    Colored box finished

--- week_03/day_3/Colored box/Colored box/MainWindow.xaml.cs
+++ week_03/day_3/Colored box/Colored box/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GreenFox;
 
 namespace Colored_box
 {
@@ -23,6 +24,22 @@ namespace Colored_box
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.LawnGreen);
+            foxDraw.DrawLine(100, 50, 200, 50);
+
+            foxDraw.StrokeColor(Colors.BlueViolet);
+            foxDraw.DrawLine(100, 100, 200, 100);
+
+            foxDraw.StrokeColor(Colors.MediumSpringGreen);
+            foxDraw.DrawLine(100, 50, 100, 100);
+
+            foxDraw.StrokeColor(Colors.MediumOrchid);
+            foxDraw.DrawLine(200, 50, 200, 100);
+
+            
+            // draw a box that has different colored lines on each edge.
         }
     }
 }
