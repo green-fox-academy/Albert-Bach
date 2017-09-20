35eea9993c1cd437ee0e7a7e91ec3937d5965828
Albert Bach <bach_albert@hotmail.hu>
2017-09-20 15:59:46 +0200

    PositionSquare finished

---
+++ week_03/day_3/PositionSquare/PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PositionSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Random random = new Random();
+            Point coord = new Point(random.Next(300), random.Next(300));
+
+            int sizeChange = 50;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Square(foxDraw, new Point(random.Next(50) +10, random.Next(50) +10), sizeChange);
+
+            }
+
+        }
+
+        public void Square(FoxDraw draw, Point coord, int sizeChange)
+        {
+            List<Point> points = new List<Point>();
+            points.Add(coord);
+            points.Add(new Point(coord.X + sizeChange, coord.Y));
+            points.Add(new Point(coord.X + sizeChange, coord.Y + sizeChange));
+            points.Add(new Point(coord.X, coord.Y + sizeChange));
+
+            draw.DrawPolygon(points);
+        }
+    }
+}
