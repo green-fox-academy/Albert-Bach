43c7b20e10e67e6e5217ac3c7ecfc19f78b86b19
Albert Bach <bach_albert@hotmail.hu>
2017-09-20 13:41:12 +0200

    HorizontalLines finished

---
+++ week_03/day_3/HorizontalLines/HorizontalLines/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace HorizontalLines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            Horizontal(foxDraw, 10, 10);
+            Horizontal(foxDraw, 20, 20);
+            Horizontal(foxDraw, 30, 30);
+
+        }
+        public void Horizontal (FoxDraw Line, int x, int y)
+        {
+            Line.DrawLine(new Point(x, y), new Point(x + 50, y));
+        }
+    }
+}
+
