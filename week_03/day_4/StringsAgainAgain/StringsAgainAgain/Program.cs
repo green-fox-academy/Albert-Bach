bd72ad91bf367d053addb27086aa4ea8f544936b
Albert Bach <bach_albert@hotmail.hu>
2017-09-21 22:46:23 +0200

    StringsAgainAgain finished

---
+++ week_03/day_4/StringsAgainAgain/StringsAgainAgain/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Write down a word !");
+            string word = Console.ReadLine();
+            Console.WriteLine(Star(word));
+            Console.ReadKey();
+
+        }
+
+        private static string Star(string word)
+        {
+            if (word.Length == 1)
+            {
+                return word;
+            }
+            else
+            {
+            return word[0] + "*" + Star(word.Substring(1));
+            }
+
+            }
+    }
+}
