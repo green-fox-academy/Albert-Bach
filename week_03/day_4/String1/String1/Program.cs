d20ad9b5936659742fca0d8b141aa630e4196a28
Albert Bach <bach_albert@hotmail.hu>
2017-09-21 21:34:17 +0200

    String modification

--- week_03/day_4/String1/String1/Program.cs
+++ week_03/day_4/String1/String1/Program.cs
@@ -13,24 +13,33 @@ namespace String1
             Console.WriteLine(" Write down a word with a letter x in it!");
             string word = Console.ReadLine();
             Console.WriteLine(Letterchanger(word));
-            Console.ReadLine();
+            Console.ReadKey();
 
         }
 
         public static string Letterchanger(string word)
         {
-            if (word.Length == 0)
+            string result;
+            int inputlength = word.Length;
+
+            if (inputlength == 0)
             {
                 return "";
             }
-            else if (word[0] == 'x')
-            {
-                return 'y' + Letterchanger(word.Substring(1));
-            }
             else
             {
-                return word[0] + Letterchanger(word.Substring(1));
+                if (word[0] == 'x')
+                {
+                    result = "y";
+                    return result + Letterchanger(word.Substring(1));
+                }
+                else
+                {
+                    return word[0] + Letterchanger(word.Substring(1));
+                }
+
             }
+
         }
     }
 }
