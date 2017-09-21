e32bd7cdfc6117a519c14ef718ee24029be2db17
Albert Bach <bach_albert@hotmail.hu>
2017-09-21 22:58:02 +0200

    The good old Fibonacci is done

---
+++ week_03/day_4/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a Fibonacci number!");
+            int number = 0;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(number));
+            Console.ReadLine();
+        }
+
+        public static int Fibonacci(int number)
+        {
+            if (number < 2)
+            {
+                return number;
+            }
+            else
+            {
+                return Fibonacci(number - 2) + Fibonacci(number - 1);
+            }
+        }
+    }
+}
