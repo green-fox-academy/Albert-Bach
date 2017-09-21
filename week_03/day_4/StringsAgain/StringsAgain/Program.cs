8424e87538fecaab9ef692580961898cede7609a
Albert Bach <bach_albert@hotmail.hu>
2017-09-21 22:23:56 +0200

    StringsAgain finished

---
+++ week_03/day_4/StringsAgain/StringsAgain/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Write down a word with a letter x in it!");
+            string word = Console.ReadLine();
+            Console.WriteLine(Remover(word));
+            Console.ReadKey();
+
+        }
+
+        private static string Remover(string word)
+        {
+            string result;
+            int wordlength = word.Length;
+
+            if (wordlength == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (word[0] == 'x')
+                {
+                    result = "";
+                    return Remover(word.Substring(1)) + result;
+                }
+                else
+                {
+                    return word[0] + Remover(word.Substring(1));
+                }
+
+            }
+        }
+    }
+}
