1125a78d0bc95a571653d8663ad570b65faa904e
Albert Bach <bach_albert@hotmail.hu>
2017-09-21 10:21:56 +0200

    Counter finished

---
+++ week_03/day_4/Counter/Counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write down a number");
+            int number = 0;
+            number= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CountDown(number));
+            Console.ReadLine();
+
+        }
+        public static int CountDown(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return CountDown(number - 1);
+            }
+        }
+
+    }
+}
