0aede80204eadd79145590c792e0afdc5be5b21d
Albert Bach <bach_albert@hotmail.hu>
2017-09-25 11:41:51 +0200

    Homework

---
+++ Homework/DoableHomework/DoableHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoableHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string tree1 = "mapple, red, 10,male";
+            string tree2 = "birch,green, 20, female";
+            string tree3 = "pine, darkgreen, 10, male";
+            string tree4 = "quaking aspen, yellow, 7, male";
+            string tree5 = " walnut, brown, 30, male";
+
+            var Trees = new List<string>() { tree1, tree2, tree3, tree4, tree5 };
+        }
+    }
+}
