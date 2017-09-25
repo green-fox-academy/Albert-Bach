7c4a39d133d51d3db33e23c0b453bbfd76be4edb
Albert Bach <bach_albert@hotmail.hu>
2017-09-25 13:27:35 +0200

    PostIt done

---
+++ week_04/day_1/Post-it/Post-it/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_it
+{
+    class Program
+    {
+
+        public struct PostIt
+        {
+            public string BackGroundColor;
+            public string Text;
+            public string TextColor;
+        }
+
+
+        static void Main(string[] args)
+        {
+            PostIt postit1;
+            postit1.BackGroundColor = "orange";
+            postit1.Text = "Idea 1";
+            postit1.TextColor = "blue";
+
+            PostIt postit2;
+            postit2.BackGroundColor = "pink";
+            postit2.Text = "Awesome";
+            postit2.TextColor = "black";
+
+            PostIt postit3;
+            postit3.BackGroundColor = "yellow";
+            postit3.Text = "Superb!";
+            postit3.TextColor = "green";
+           
+        }
+    }
+}
