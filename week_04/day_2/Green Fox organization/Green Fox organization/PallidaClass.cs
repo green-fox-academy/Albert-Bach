f863f4631ad6f08623c37c9f669998170eab35ab
Albert Bach <bach_albert@hotmail.hu>
2017-09-26 17:36:03 +0200

    3/4 academy

---
+++ week_04/day_2/Green Fox organization/Green Fox organization/PallidaClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_organization
+{
+    class PallidaClass
+    {
+
+        string className;
+        List <Student> students  = new List<Student>();
+        List <Mentor> mentors = new List<Mentor>();
+
+
+    public void AddStudent(Student student)
+    {
+            students.Add(student);
+    }
+
+    public void AddMentor(Mentor mentor)
+    {
+            mentors.Add(mentor);
+    }
+
+    public void Info()
+    {
+            Console.WriteLine("Pallida"+ className+ "class has"+ students.size + "students" + "and"+ mentors.size + "mentors.");
+    }
+
+    }
+}
---
+++ week_04/day_2/Green Fox organization/Green Fox organization/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_organization
+{
+    class Person
+    {
+            
+        public string name;
+        public int age;
+        public string gender;
+
+        public Person(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm" + name + age + "year old" + gender);
+        }
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+
+        public Person()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+        }
+      
+    }
+}
