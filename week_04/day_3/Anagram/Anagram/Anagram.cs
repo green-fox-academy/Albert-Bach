49d6049787cf851cf2925c7b348366eb25337174
Albert Bach <bach_albert@hotmail.hu>
2017-09-27 16:16:38 +0200

    Anagram done

---
+++ week_04/day_3/Anagram/Anagram/Anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class Anagrammas
+    {
+        public bool IsAnagram(string word1, string word2)
+        {
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+            char[] first = word1.ToLower().ToCharArray();
+            char[] second = word2.ToLower().ToCharArray();
+
+            Array.Sort(first);
+            Array.Sort(second);
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
---
+++ week_04/day_3/Anagram/AnagramTest/AnagramTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+using NUnit.Framework;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTest
+    {
+        Anagrammas anagrams = new Anagrammas();
+
+        [Test]
+        public void AnagramTester()
+        {
+            // arrange
+            string word1 = "fire";
+            string word2 = "earth";
+            
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, anagrams.IsAnagram(word1,word2));
+        }
+    }
+}
