d57cd51a2b26512fb2aab5dbdac98a01f5a6b744
Albert Bach <bach_albert@hotmail.hu>
2017-09-27 15:21:25 +0200

    Sum done

---
+++ week_04/day_3/Sum/Sum/Sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumnumbers
+{
+    public class Sum
+    {
+       
+        public int SumMethod(List <int> numbers)
+        {
+            if (numbers != null)
+            {
+                return numbers.Sum();
+            }
+            else
+            {
+                return 5;
+            }
+        }
+
+    }
+}
---
+++ week_04/day_3/Sum/SumTest/SumTest.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sumnumbers;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumTest
+    {
+        //[Test]
+        //public void SumNumbersTest()
+        //{
+        //    //arrange
+        //    List<int> numbers = new List<int> { 1, 2, 3 };
+        //    var Sum =  numbers;
+
+        //    //act
+        //    int output = 4;
+
+        //    //assert
+        //    Assert.AreEqual(numbers.Sum(), output);
+        //}
+        [Test]
+        public void SumNumbersTest()
+        {
+            //arrange
+            List<int> numbers = new List<int> { 1, 2, 3 };
+            Sum SumClass = new Sum();
+
+            //act
+            int output = 6;
+
+            //assert
+            Assert.AreEqual(SumClass.SumMethod(numbers), output);
+        }
+        [Test]
+        public void SumEmptyTest()
+        {
+            //arrange
+            List<int> numbers = new List<int> { };
+            Sum SumClass = new Sum();
+
+            //act
+            int output = 0;
+
+            //assert
+            Assert.AreEqual(SumClass.SumMethod(numbers), output);
+        }
+
+        [Test]
+        public void SumOneTest()
+        {
+            //arrange
+            List<int> numbers = new List<int> { 5 };
+            Sum SumClass = new Sum();
+
+            //act
+            int output = 5;
+
+            //assert
+            Assert.AreEqual(SumClass.SumMethod(numbers), output);
+        }
+
+        [Test]
+        public void SumEmpty()
+        {
+            //arrange
+            Sum SumClass = new Sum();
+
+            //act
+            int output = 5;
+
+            //assert
+            Assert.AreEqual(SumClass.SumMethod(null), output);
+        }
+    }
+}
