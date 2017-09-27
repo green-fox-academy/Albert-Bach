67944e529f2a49887923d0d0706a2064ab5cf3be
Albert Bach <bach_albert@hotmail.hu>
2017-09-27 17:34:12 +0200

    CountLetters done

---
+++ week_04/day_3/CountLetters/CountLetters/CountLetters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class CountLet
+    {
+        public Dictionary<char, int> LetterCounter(string word)
+        {
+            Dictionary<char, int> resultDictionary = new Dictionary<char, int>();
+            int counter = 1;
+            word.ToCharArray();
+
+            foreach (char letter in word)
+            {
+                if (!resultDictionary.ContainsKey(letter))
+                {
+                    resultDictionary.Add(letter, counter);
+                }
+                else
+                {
+                   resultDictionary[letter]++;
+                }
+            }
+
+            return resultDictionary;
+        }
+    }
+}
---
+++ week_04/day_3/CountLetters/CountLettersTest/CountLettersTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountLetters;
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class CountLettersTest
+    {
+        CountLet countIt = new CountLet();
+
+        [Test]
+        public void DictionaryTester()
+        {
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+            { 'e', 1},
+            { 't', 1},
+            { 'h', 1},
+            { 'a', 1},
+            { 'r', 1}
+            };
+            Assert.AreEqual(output, countIt.LetterCounter("earth"));
+        }
+    }
+}
