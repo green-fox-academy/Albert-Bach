daea34d83e2de6b08e54fc8683ec620a762a878f
Albert Bach <bach_albert@hotmail.hu>
2017-09-27 13:28:31 +0200

    Apples Finally done

---
+++ week_04/day_3/ClassLibrary1/AppleTest/AppleTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apples;
+
+namespace AppleTest
+{
+    [TestFixture]
+    public class AppleTest
+    {
+        [Test]
+        public void GetAppleTest()
+        {
+            string fruit = "Apple";
+            var apple = new Apple(fruit);
+
+            string output = apple.GetApple();
+
+            Assert.AreEqual(fruit, output);
+        }
+
+        [Test]
+        public void GetPearTest()
+        {
+            string fruit = "Pear";
+            var apple = new Apple(fruit);
+
+            string output = apple.GetApple();
+
+            Assert.AreEqual(fruit, output);
+        }
+
+        [Test]
+        public void GetApplesTest()
+        {
+            string fruit = "Apple";
+            var apple = new Apple(fruit);
+
+            string output = apple.GetApple();
+
+            Assert.AreEqual(fruit, output);
+        }
+    }
+}
---
+++ week_04/day_3/ClassLibrary1/ClassLibrary1/Apple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Apples
+{
+    public class Apple
+    {
+        public string fruit;
+
+        public Apple(string fruit)
+        {
+            this.fruit = "Apple";
+        }
+
+        public string GetApple()
+        {
+            return fruit;
+        }
+    }
+}
