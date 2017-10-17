31c5402a8291e61e63c5a6052cf7bc78ac6b7aa9
Albert Bach <bach_albert@hotmail.hu>
2017-10-17 17:39:45 +0200

    Comparable feat

---
+++ week_06/day_2/Comparable/Comparable/Domino.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Comparable
+{
+	public class Domino : IComparable
+	{
+		private readonly int[] Values;
+
+		public Domino(int valueA, int valueB)
+		{
+			this.Values = new int[] { valueA, valueB };
+		}
+
+		public int CompareTo(object obj)
+		{
+			//Domino otherDomino = obj as Domino;
+			Domino otherDomino = (Domino)obj;
+			return Values[0].CompareTo(otherDomino.Values[0]);
+		}
+
+		public override string ToString()
+		{
+			return Values[0].ToString() + "," + Values[1];
+		}
+
+		public int[] GetValues()
+		{
+			return Values;
+		}
+	}
+}
