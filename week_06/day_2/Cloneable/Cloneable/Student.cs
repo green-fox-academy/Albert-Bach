588696b517fccb82e8a110c7965a40f3c6d19b4d
Albert Bach <bach_albert@hotmail.hu>
2017-10-17 13:53:46 +0200

    Cloneable feat

---
+++ week_06/day_2/Cloneable/Cloneable/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cloneable
+{
+	class Student : ICloneable
+	{
+		string name;
+		int age;
+		string gender;
+		string previousOrganization;
+
+		public Student(string name, int age, string gender, string previousOrganization)
+		{
+			this.name = name;
+			this.age = age;
+			this.gender = gender;
+			this.previousOrganization = previousOrganization;
+		}
+
+		public void Introduce()
+		{
+			Console.WriteLine("{0} {1} {2} {3}", name, age, gender, previousOrganization);
+		}
+
+		public object Clone()
+		{
+			return MemberwiseClone();
+		}
+
+	}
+}
