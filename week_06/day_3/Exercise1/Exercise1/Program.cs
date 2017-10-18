7f680b1993011089f42d00bd88aedd1632e3ce80
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 11:23:37 +0200

    Exercise1 feat

---
+++ week_06/day_3/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+			IEnumerable<int> numbersQuery =
+				from evenNumbers in n
+				where evenNumbers / 2 == 0
+				select evenNumbers;
+			
+			IEnumerable<int> numbersQueryWithLambda = n.Where(y => y % 2 == 0);
+
+			foreach (var number in numbersQueryWithLambda)
+			{
+				Console.WriteLine(number);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
