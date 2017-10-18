f7a81c44c6702f6e81744f4872fa995e02a087f6
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 16:54:56 +0200

    Exercise7 feat

--- week_06/day_3/Exercise7/Exercise7/Program.cs
+++ week_06/day_3/Exercise7/Exercise7/Program.cs
@@ -10,6 +10,20 @@ namespace Exercise7
 	{
 		static void Main(string[] args)
 		{
+			string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+			var startsA = 
+			from word in cities
+			where word.StartsWith("A")
+			where word.EndsWith("I")
+			select word;
+
+			var startsAWithLambda = cities.Where(y => y.StartsWith("A")).Where(y => y.EndsWith("I"));
+
+			foreach (var item in startsAWithLambda)
+			{
+				Console.WriteLine(item);
+			}
+			Console.ReadLine();
 		}
 	}
 }
