888fd044462878a836ac5dfee355f525c5eb7efb
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 16:48:13 +0200

    Exercise 6 feat

---
+++ week_06/day_3/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string word = "characters";
+
+			var frequencyOfCharacters = from character in word
+										group character by character into chars
+										select new { chars.Key, Count = chars.Count() };
+
+			var frequency = word.GroupBy(y => y).ToDictionary(y => y.Key, y => y.Count());
+
+			foreach (var letter in frequency)
+			{
+				Console.WriteLine(letter);
+			}
+			Console.ReadLine();
+		}
+	}
+}
