80ccaf53dd4bc7a908a24fef741a96d4e6733e91
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 11:46:16 +0200

    Exercise2 feat

---
+++ week_06/day_3/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+			IEnumerable<int> oddAvarageQuery =
+			from oddAvarage in n
+			where oddAvarage % 2 != 0 
+			select oddAvarage;
+
+			var oddAvarageWithLambda = n.Where(y => y % 2 != 0).Average();
+
+			Console.WriteLine(oddAvarageQuery.AsQueryable().Average());
+			Console.WriteLine(oddAvarageWithLambda);
+			Console.ReadLine();
+		}
+	}
+}
