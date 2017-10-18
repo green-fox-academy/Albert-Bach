645a8ec0064a7dea6ae012fa0de5b092d48e891a
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 15:07:27 +0200

    Exercise 5 feat

--- week_06/day_3/Exercise5/Exercise5/Program.cs
+++ week_06/day_3/Exercise5/Exercise5/Program.cs
@@ -10,6 +10,21 @@ namespace Exercise5
 	{
 		static void Main(string[] args)
 		{
+			int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+			var frequenciesOfNumbers =
+			from square in n
+			orderby square ascending
+			group square by square into uniqueSquare
+			select new { uniqueSquare.Key, Count = uniqueSquare.Count() };
+
+			var frequency = n.GroupBy(y => y).ToDictionary(y => y.Key, y => y.Count());
+
+			foreach (var nums in frequenciesOfNumbers)
+			{
+				Console.WriteLine(nums);
+			}
+			Console.ReadLine();
 		}
 	}
 }
