d3c40ddb26179b9d81d2237ce17d8eb7ca57f593
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 17:01:01 +0200

    Exercise8 feat

---
+++ week_06/day_3/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string word = "London";
+
+			var upperCase =
+			from character in word.ToArray()
+			where Char.IsUpper(character)
+			select character;
+
+			var upperCaseLambda = word.Where(y => char.IsUpper(y));
+
+			foreach (var character in upperCaseLambda)
+            {
+				Console.WriteLine(character);
+			}
+			Console.ReadLine();
+		}
+	}
+}
