97a1ec3174cfed949295b79a7bbf8520ec6c2ed3
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 13:04:02 +0200

    Exercise4 feat

---
+++ week_06/day_3/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+			IEnumerable<int> squaresQuery =
+			from square in n
+			where square * square > 20
+			select square ;
+
+			foreach (var number in squaresQuery)
+			{
+				Console.WriteLine(number);
+			}
+
+			IEnumerable<int> squaresWithLambda = n.Where(y => y * y > 20);
+
+
+			Console.ReadLine();
+		}
+	}
+}
