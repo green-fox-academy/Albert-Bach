ce6e534e013313420868a64f48c3c9eaacff6c28
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 17:19:06 +0200

    Exercise9 feat

---
+++ week_06/day_3/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] letters = { 'L', 'o', 'n', 'd', 'o', 'n' };
+
+			var tostringWithLambda = String.Join("", letters.Select(o => o.ToString()).ToArray());
+
+			Console.WriteLine(tostringWithLambda);
+			Console.ReadLine();
+		}
+	}
+}
