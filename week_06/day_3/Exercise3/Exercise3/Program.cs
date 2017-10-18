e50de488c3dd9197d431cd6073def7767f7ec0b0
Albert Bach <bach_albert@hotmail.hu>
2017-10-18 12:55:42 +0200

    Exercise3 feat

---
+++ week_06/day_3/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+			IEnumerable<int> positiveSquaresQuery =
+			from positiveSquare in n
+			where positiveSquare > 0
+			select positiveSquare * positiveSquare;
+
+			foreach (var number in positiveSquaresQuery)
+			{
+				Console.WriteLine(number);
+			}
+
+			var positiveSquaresWithLambda = n.Where(y => y > 0).Select(y => y * y);
+
+			Console.ReadLine();
+		}
+	}
+}
