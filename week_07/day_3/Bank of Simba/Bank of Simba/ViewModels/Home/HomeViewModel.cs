6afd6f427730b5a651eb9bcb525c43f69e51244c
Albert Bach <bach_albert@hotmail.hu>
2017-10-25 17:18:56 +0200

    fix (BankOfSimba)

--- week_07/day_3/Bank of Simba/Bank of Simba/ViewModels/Home/HomeViewModel.cs
+++ week_07/day_3/Bank of Simba/Bank of Simba/ViewModels/Home/HomeViewModel.cs
@@ -9,13 +9,13 @@ namespace BankOfSimba.ViewModels.Home
 	public class HomeViewModel
 	{
 
-		public List<BankAccount> accounts { get; set; } = new List<BankAccount>();
-
-		public BankAccount bankAccount1 { get; set; } = new BankAccount("Simba", 2000, "Lion", true, true);
-		public BankAccount bankAccount2 { get; set; } = new BankAccount("Shenzi", 1500, "Hyena",false, false);
-		public BankAccount bankAccount3 { get; set; } = new BankAccount("Timon", 500, "Meerkat",false, true);
-		public BankAccount bankAccount4 { get; set; } = new BankAccount("Pumba", 1000, "Warthog",false, true);
-		public BankAccount bankAccount5 { get; set; } = new BankAccount("Rafiki", 3000, "Monkey",false, true);
-
+		public List<BankAccount> accounts { get; set; } = new List<BankAccount>
+		{
+		 new BankAccount("Simba", 2000, "Lion", true, true),
+		 new BankAccount("Shenzi", 1500, "Hyena", false, false),
+		 new BankAccount("Timon", 500, "Meerkat", false, true),
+		 new BankAccount("Pumba", 1000, "Warthog", false, true),
+		 new BankAccount("Rafiki", 3000, "Monkey", false, true)
+		};
 	}
 }
