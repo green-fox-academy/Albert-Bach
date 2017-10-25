46a9b0224104ca13abf40bc3f28867767ee5ae60
Albert Bach <bach_albert@hotmail.hu>
2017-10-25 13:59:52 +0200

    feat(BankOfSimba) part2

--- week_07/day_3/Bank of Simba/Bank of Simba/Models/BankAccount.cs
+++ week_07/day_3/Bank of Simba/Bank of Simba/Models/BankAccount.cs
@@ -3,7 +3,7 @@
 	public class BankAccount
 	{
 		public string Name { get; set; } = "Simba";
-		public int Balance { get; set; } = 2000;
+		public double Balance { get; set; } = 2000;
 		public string AnimalType { get; set; } = "Lion";
 	}
 }
