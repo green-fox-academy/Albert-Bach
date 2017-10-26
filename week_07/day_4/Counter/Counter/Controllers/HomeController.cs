58f7edbeb4b12647c036eefc01cb6921e42fea50
Albert Bach <bach_albert@hotmail.hu>
2017-10-26 13:32:04 +0200

    feat (Counter)

---
+++ week_07/day_4/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CounterApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CounterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+		Counter counter;
+
+		public HomeController (Counter counter)
+		{
+			this.counter = counter;
+		}
+
+		[HttpGet]
+		[Route ("Index")]
+		public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+		[HttpPost]
+		[Route("Index")]
+		public IActionResult Increase()
+		{
+			counter.Raise();
+			return RedirectToAction("Index");
+		}
+	}
+}
