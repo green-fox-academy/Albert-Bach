1da1c3a39d8432d3b64d41c81397da754114679a
Albert Bach <bach_albert@hotmail.hu>
2017-10-31 14:27:25 +0100

    feat (ListingTodos) classes done

---
+++ week_08/day_2/ListingTodos/ListingTodos/Controllers/TodoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ListingTodos.Controllers
+{
+    [Route("/todo")]
+    public class TodoController : Controller
+    {
+        //public TodoController(StudentRepository studentRepository)
+        //{
+        //    StudentRepository = studentRepository;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/")]
+        [Route("/list")]
+        public IActionResult List()
+        {
+            string words = "This is my first todo";
+            return View((object)words);
+        }
+    }   
+}
