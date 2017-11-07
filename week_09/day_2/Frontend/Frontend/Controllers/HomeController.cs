0f4e62225377060a100a678bceace8c69e8bf500
Albert Bach <bach_albert@hotmail.hu>
2017-11-07 11:53:41 +0100

    feat (Frontend)

---
+++ week_09/day_2/Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        //[Route("/doubling")]
+        //[HttpGet]
+        //public IActionResult Doubling(int input)
+        //{
+        //    "received": 15;
+        //    "result": 30;
+        //    return View();
+        //}
+    }
+}
